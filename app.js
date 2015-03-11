6dd8e31cb72ca8a33c4a42412f4aa2286afcf882
dathims <dathims@gmail.com>
2015-03-11 23:55:28 +0100

    procfile fix mv

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ var port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + '/dist'));
 app.listen(port);
+console.log('Example app listening at http://:%s', port);
